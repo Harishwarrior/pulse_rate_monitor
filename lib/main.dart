94b7628b0b2fe1c0607a1c635338f11fcd85064d
harishwarrior <warriorharish95668@gmail.com>
2020-11-29 15:18:53 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:PPG/view/home_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PPG',
+      theme: ThemeData(
+        brightness: Brightness.light,
+      ),
+      home: HomePage(),
+    );
+  }
+}
