ec416ef3a1449d726f3a4128490c9c9b5aac5c20
SarinSthapit <sarinsthapit.2001@gmail.com>
2022-10-09 20:34:49 +0545

    Improved documentation

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ref.watch(isDarkModeProvider) ? lightTheme : darkTheme,
       home: HomePage(),
     );
